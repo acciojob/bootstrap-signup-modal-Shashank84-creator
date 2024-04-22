05129ad2ac27bad6e9e33a568e69aacc81b6edbb
Shashank sahu <144484964+Shashank84-creator@users.noreply.github.com>
2024-04-22 19:28:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,7 @@
   cy.get('button').first().click().then(() => {
     cy.get('.modal').should('be.visible');
     cy.get('.close').click().then(() => {
-      cy.wait(1000); // wait for 1 second
-      cy.get('.modal').should('not.be.visible');
+      cy.get('.modal', { timeout: 10000 }).should('not.be.visible'); // wait for 10 seconds
     });
   });
 }
