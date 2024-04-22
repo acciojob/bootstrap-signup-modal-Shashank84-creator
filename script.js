bfec55f43079ecaef15c2cd0eaecc32ee40bb16f
Shashank sahu <144484964+Shashank84-creator@users.noreply.github.com>
2024-04-22 19:33:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,10 @@
     });
   });
 }
+() => {
+  cy.get('button').first().click().then(() => {
+    cy.get('.modal').should('be.visible');
+    cy.get('.modal').trigger('hide.bs.modal');
+    cy.get('.modal', { timeout: 10000 }).should('not.be.visible'); // wait for 10 seconds
+  });
+}
