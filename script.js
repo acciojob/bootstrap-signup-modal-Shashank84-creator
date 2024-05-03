5c3dc090d98c8d181605e693df188f7c4d344ecc
Shashank sahu <144484964+Shashank84-creator@users.noreply.github.com>
2024-05-03 16:10:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 cy.get('button').first().click().then(() => {
   cy.get('.modal').should('be.visible');
-  cy.get('.close').click({ force: true }).then(() => 
-    cy.get('.modal').should('not.be.visible')
-  );
+  cy.get('.close').click({ force: true });
+  cy.wait(500); // wait for 500ms to ensure the modal is closed
+  cy.get('.modal').should('not.be.visible');
 });
