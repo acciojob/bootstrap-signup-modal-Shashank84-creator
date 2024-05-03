d17e652a5a555c9e578f571914c039e9349b2e56
Shashank sahu <144484964+Shashank84-creator@users.noreply.github.com>
2024-05-03 15:57:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-cy.get('button').first().click().then(() => {
-  cy.get('.modal').should('be.visible');
-  cy.get('.close').then(($close) => {
-    expect($close).to.have.length(1);
-    cy.wrap($close).click({ force: true });
-    cy.wait(1000);  // adjust the delay as per your need
+() => {
+  cy.get('button').first().click().then(() => {
+    cy.get('.modal').should('be.visible');
+    cy.get('.close').click();
+    cy.wait(1000); // Wait for 1 second
     cy.get('.modal').should('not.be.visible');
   });
-});
+}
