5b48e2fb0102ab72debefee636adc64feb79f831
Shashank sahu <144484964+Shashank84-creator@users.noreply.github.com>
2024-05-03 16:07:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 cy.get('button').first().click().then(() => {
-  cy.get('.modal').should('exist').and('be.visible');
-  cy.get('.close').click().then(() => 
+  cy.get('.modal').should('be.visible');
+  cy.get('.close').click({ force: true }).then(() => 
     cy.get('.modal').should('not.be.visible')
   );
 });
