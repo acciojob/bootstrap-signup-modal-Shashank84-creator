09105560207e86436da641ae36f1d2ac8dc630dd
Shashank sahu <144484964+Shashank84-creator@users.noreply.github.com>
2024-05-03 16:23:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ cy.get('button').first().click().then(() => {
   cy.get('.modal').should('be.visible');
   cy.get('.close').click({ force: true });
   cy.waitUntil(() =>!cy.get('.modal').hasClass('show'));
+  cy.get('.modal').should('not.be.visible');
+});
+
+cy.get('button').first().click().then(() => {
+  cy.get('.modal').should('be.visible');
+  cy.get('.close').click({ force: true });
+  cy.waitUntil(() =>!cy.get('.modal').hasClass('show'));
   cy.get('.modal').should('not.be.visible', { timeout: 6000 });
 });
-// cy.get('button').first().click().then(() => {
-//   cy.get('.modal').should('be.visible');
-//   cy.get('.close').click({ force: true });
-//   cy.log('Close button clicked');
-//   cy.waitUntil(() =>!cy.get('.modal').hasClass('show'));
-//   cy.log('Waiting for modal to be closed');
-//   cy.get('.modal').should('not.be.visible');
-//   cy.log('Modal should not be visible');
-// });
+cy.get('button').first().click().then(() => {
+  cy.get('.modal').should('be.visible');
+  cy.get('.close').click({ force: true });
+  cy.log('Close button clicked');
+  cy.waitUntil(() =>!cy.get('.modal').hasClass('show'));
+  cy.log('Waiting for modal to be closed');
+  cy.get('.modal').should('not.be.visible');
+  cy.log('Modal should not be visible');
+});
