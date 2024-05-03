123795a9b58d6a05f7ff0a0377f588e04f7339ca
Shashank sahu <144484964+Shashank84-creator@users.noreply.github.com>
2024-05-03 16:18:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 cy.get('button').first().click().then(() => {
   cy.get('.modal').should('be.visible');
   cy.get('.close').click({ force: true });
-  cy.log('Close button clicked');
   cy.waitUntil(() =>!cy.get('.modal').hasClass('show'));
-  cy.log('Waiting for modal to be closed');
-  cy.get('.modal').should('not.be.visible');
-  cy.log('Modal should not be visible');
+  cy.get('.modal').should('not.be.visible', { timeout: 6000 });
 });
+// cy.get('button').first().click().then(() => {
+//   cy.get('.modal').should('be.visible');
+//   cy.get('.close').click({ force: true });
+//   cy.log('Close button clicked');
+//   cy.waitUntil(() =>!cy.get('.modal').hasClass('show'));
+//   cy.log('Waiting for modal to be closed');
+//   cy.get('.modal').should('not.be.visible');
+//   cy.log('Modal should not be visible');
+// });
