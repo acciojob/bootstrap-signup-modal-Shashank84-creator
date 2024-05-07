b1be1b292d7d27197b2327c0ac899f41093d2885
Shashank sahu <144484964+Shashank84-creator@users.noreply.github.com>
2024-05-07 20:27:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+// () => { 
+//   cy.get('button').first().click().then(() => { 
+//     cy.get('.modal').should('be.visible'); 
+//     cy.get('.close').click().then(() => {
+//       // cy.wait(1000); // Adding a small delay to ensure modal is fully closed
+//       // cy.get('.modal').should('not.be.visible');
+// 		cy.get().should('not.exist')
+// 		cy.get('.modal', { timeout: 4000 }).should('not.be.visible');
+//     });
+//   }); 
+// }
 () => { 
   cy.get('button').first().click().then(() => { 
     cy.get('.modal').should('be.visible'); 
-    cy.get('.close').click().then(() => {
-      // cy.wait(1000); // Adding a small delay to ensure modal is fully closed
-      // cy.get('.modal').should('not.be.visible');
-		cy.get().should('not.exist')
-		cy.get('.modal', { timeout: 4000 }).should('not.be.visible');
-    });
+    cy.get('.close').click();
+    cy.wait(2000); // wait for 2 seconds
+    cy.get('.modal').should('not.be.visible');
   }); 
 }
