9dbe5572be974d5efa8b196fc9840223d3014100
Shashank sahu <144484964+Shashank84-creator@users.noreply.github.com>
2024-05-07 20:19:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
       // cy.wait(1000); // Adding a small delay to ensure modal is fully closed
       // cy.get('.modal').should('not.be.visible');
 		cy.get().should('not.exist')
-		cy.get('.modal', { timeout: 10000 }).should('not.be.visible');
+		cy.get('.modal', { timeout: 4000 }).should('not.be.visible');
     });
   }); 
 }
